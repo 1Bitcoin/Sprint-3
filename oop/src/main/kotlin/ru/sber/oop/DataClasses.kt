78ddefdc221ead33ccfced65f0b6d5d2deaf016d
Дмитрий l W.W <shigalkin@mail.ru>
2021-09-01 18:49:56 +0300

    DataClasses fix

--- oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
+++ oop/src/main/kotlin/ru/sber/oop/DataClasses.kt
@@ -4,13 +4,19 @@ data class User(val name: String, val age: Long) {
     lateinit var city: String
 
     override fun equals(other: Any?): Boolean {
-        return other is User && other.city == city && other.age == age && other.name == name
+        if (other !is User) return false
+        var ret = false
+
+        if (::city.isInitialized && other::city.isInitialized) {
+            ret = other.age == age && other.name == name && other.city == city
+        }
+
+        return ret
     }
 
     override fun hashCode(): Int {
         return city.hashCode() xor name.hashCode() xor age.hashCode()
     }
-
 }
 
 fun main() {
