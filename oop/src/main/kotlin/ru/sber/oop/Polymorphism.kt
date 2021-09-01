3da6efe7c0ef4e3f86b35552d48f26e28d78ae66
Дмитрий l W.W <shigalkin@mail.ru>
2021-09-01 20:49:32 +0300

    Polymorphism done

--- oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
+++ oop/src/main/kotlin/ru/sber/oop/Polymorphism.kt
@@ -1,13 +1,49 @@
 package ru.sber.oop
 
+import kotlin.random.Random
+
 interface Fightable {
     val powerType: String
     var healthPoints: Int
     val damageRoll: Int
+        get() = (0..10).random()
 
     fun attack(opponent: Fightable): Int
 }
 
-//TODO: create class Player, Monster, Goblin here...
+class Player(val name: String, val isBlessed: Boolean): Fightable {
+    override val powerType: String
+        get() = TODO("Not yet implemented")
 
+    override var healthPoints: Int = 100
 
+    override fun attack(opponent: Fightable): Int {
+        var totalDamage = damageRoll
+        if (isBlessed)
+            totalDamage = damageRoll * 2
+
+        opponent.healthPoints -= totalDamage
+        return totalDamage
+    }
+}
+
+abstract class Monster(val name: String, val description: String): Fightable {
+    override fun attack(opponent: Fightable): Int {
+        opponent.healthPoints -= damageRoll
+        return damageRoll
+    }
+}
+
+fun Monster.getSalutation() = "Hello, I'm Monster"
+
+class Goblin(): Monster("Zombie", "Bad boy") {
+    override val powerType: String
+        get() = TODO("Not yet implemented")
+
+    override var healthPoints: Int
+        get() = TODO("Not yet implemented")
+        set(value) { healthPoints = value }
+
+    override val damageRoll: Int
+        get() = super.damageRoll / 2
+}
