4ce3f37eecbd6299933bbbefa565312cc40a9975
Дмитрий l W.W <shigalkin@mail.ru>
2021-09-03 23:46:24 +0300

    add test DayOfWeek

--- qa/src/test/HrDepartmentTest.kt
+++ qa/src/test/HrDepartmentTest.kt
@@ -1,25 +1,50 @@
+package ru.sber.qa
+
+import io.mockk.every
+import io.mockk.mockkClass
+import io.mockk.mockkStatic
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.time.*
 
-import org.junit.jupiter.api.Assertions.*
 
 internal class HrDepartmentTest {
-
     @BeforeEach
     fun setUp() {
-
+        mockkStatic(LocalDateTime::class)
     }
 
-    @AfterEach
-    fun tearDown() {
+    @ParameterizedTest
+    @MethodSource("receiveRequestParams")
+    fun receiveRequest(currentDay: DayOfWeek, certificateRequest: CertificateRequest) {
+
+        // given
+        every { LocalDateTime.now(Clock.systemUTC()).dayOfWeek } returns currentDay
+
+        // result
+        Assertions.assertThrows(WeekendDayException::class.java) { HrDepartment.receiveRequest(certificateRequest) }
     }
 
-    @Test
-    fun receiveRequest() {
+    companion object {
+        @JvmStatic
+        fun receiveRequestParams() = listOf(
+                Arguments.of(DayOfWeek.SATURDAY, CertificateRequest(23L, CertificateType.NDFL)),
+                Arguments.of(DayOfWeek.SUNDAY, CertificateRequest(2222L, CertificateType.LABOUR_BOOK))
+        )
     }
 
     @Test
     fun processNextRequest() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+
     }
 }
