ee05168649c7276cb2f84b013eb3bbb0e6b9d224
Дмитрий l W.W <shigalkin@mail.ru>
2021-09-05 18:15:04 +0300

    done archivator

--- io/src/main/kotlin/ru/sber/io/Archivator.kt
+++ io/src/main/kotlin/ru/sber/io/Archivator.kt
@@ -1,5 +1,11 @@
 package ru.sber.io
 
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
+
 /**
  * Реализовать методы архивации и разархивации файла.
  * Для реализиации использовать ZipInputStream и ZipOutputStream.
@@ -11,7 +17,25 @@ class Archivator {
      * Архив должен располагаться в том же каталоге, что и исходной файл.
      */
     fun zipLogfile() {
+        val originalFile = "logfile.log"
+        val resultFile = "logfile.zip"
 
+        try {
+            val zipOutputStream = ZipOutputStream(FileOutputStream(resultFile))
+            val fileInputStream = FileInputStream(originalFile)
+            var fileBuffer: ByteArray
+            val zipEntry = ZipEntry(originalFile)
+
+            fileInputStream.use { fileBuffer = fileInputStream.readBytes() }
+
+            zipOutputStream.use {
+                zipOutputStream.putNextEntry(zipEntry)
+                zipOutputStream.write(fileBuffer)
+                zipOutputStream.closeEntry()
+            }
+        } catch (exception: Exception) {
+            println(exception.message)
+        }
     }
 
     /**
@@ -19,6 +43,24 @@ class Archivator {
      * Извлечь из архива logfile.zip файл и сохарнить его в том же каталоге с именем unzippedLogfile.log
      */
     fun unzipLogfile() {
+        val originalFile = "logfile.zip"
+        val resultFile = "logfile.log"
 
+        try {
+            val zipInputStream = ZipInputStream(FileInputStream(originalFile))
+            val fileOutputStream = FileOutputStream(resultFile)
+            var fileBuffer: ByteArray
+
+            zipInputStream.use {
+                zipInputStream.nextEntry
+                fileBuffer = zipInputStream.readBytes()
+                zipInputStream.closeEntry()
+            }
+
+            fileOutputStream.use { fileOutputStream.write(fileBuffer) }
+
+        } catch (exception: Exception) {
+            println(exception.message)
+        }
     }
 }
